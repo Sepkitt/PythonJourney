6faeb6748e46e15a645d02866aa51a9552276c4e
Die_Bliksem <99402948+Sepkitt@users.noreply.github.com>
2022-03-21 09:14:58 -0700

    Changed Game's story to make it slightly more interesting

--- Beginner Projects/Text-based Adventure Game/main.py
+++ Beginner Projects/Text-based Adventure Game/main.py
@@ -2,7 +2,7 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
-play = input('Do you want to play? Y or N ').lower()
+answer = input('Do you want to play? (yes / no) ').lower().strip()
 print ('Lets begin!')
     
     
@@ -10,25 +10,27 @@ print ('Lets begin!')
 def start_game():
     healthPoints = 10
 
-    print(f' {Fore.GREEN}your starting with {healthPoints} health points')
+    print(f'You are an elite soldier tasked with saving the princess, your starting with {Fore.GREEN} {healthPoints}{Fore.RESET} health points')
 
-    choice = input(f'your in a room that has two doors, do you go to the blue door or  yellow door? Ans {Fore.YELLOW}Y {Fore.RESET} or  {Fore.BLUE}B ').lower()
+    answer = input(f'you are standing on a platform, and a monster is chasing you from the left side, do you go left or right? ( {Fore.YELLOW}left {Fore.RESET} or  {Fore.BLUE} right )').lower().strip()
     
-    while True:
-        if(choice == 'Y'):
-            print(f'you chose to through the Yellow door')
-        elif(choice == 'B'):
+    while healthPoints > 1:
+        if(answer == 'left'):
+            healthPoints = 0
+            print(f'you chose to left and the monster eats you')
+        elif(answer == 'right'):
             print(f'you chose to go through the Blue door')
     
         else:
-            print('Please choose Y or B')
+            print('Please choose left or right')
             start_game()   
-        
+            
+    print(' You have run out of health and has sadly passed away, rest in peace soldier')   
     
     
     
     
-if(play == 'y'):
+if(answer == 'yes'):
     
     start_game()
     
