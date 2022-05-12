61f0bc844bf6a70be91c7ef0daf0252972e6bcf4
Jermaine Sepkitt <99402948+Sepkitt@users.noreply.github.com>
2022-05-12 13:40:24 +0200

    Updated Contacted File

--- Beginner Projects/Contact_Book/main.py
+++ Beginner Projects/Contact_Book/main.py
@@ -1,6 +1,17 @@
 #The objective of this project is to create an Address book using python in which the user can add a new contact 
 #edit and delete existing contact and view all the contact.
 
+with open('contact.txt', 'a') as f:
+    name = input('Name: ')
+    phone = input('Phone: ')
+    f.writelines(f'Name: {name} \nPhone: {phone} \n\n')
+    
+
+
+# user prompt
+
+
+
 
 # Add contact
 
